37d9ac7c1c40ffb2930a6c887a9cf06229ace315
Iara Barcos <iarabarcos19@hotmail.com>
2023-08-09 10:37:59 -0300

    modificado

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,55 +1,114 @@
-class ProductManager {
-    constructor() {
-      this.products = [];
-      this.currentId = 1;
-    }
-  
-    addProduct(product) {
-      const newProduct = {
-        id: this.currentId,
-        ...product
-      };
-      this.products.push(newProduct);
-      this.currentId++;
-      console.log("Producto agregado:", newProduct);
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find(p => p.id === id);
-      if (product) {
-        return product;
+import fs from "fs/promises";
+
+export default class ProductManager {
+  id = 0;
+  constructor() {
+    this.path = "./src/productos.json";
+    this.productos = [];
+    this.loadData();
+  }
+
+  async loadData() {
+    try {
+      const json = await fs.readFile(this.path, "utf-8");
+      this.productos = JSON.parse(json);
+      if (this.productos.length < 1) {
+        this.id = 1;
       } else {
-        console.log("Producto no encontrado.");
+        this.id = this.productos[this.productos.length - 1].id + 1;
       }
-    }
-  
-    updateProduct(id, updatedProduct) {
-      const productIndex = this.products.findIndex(p => p.id === id);
-      if (productIndex !== -1) {
-        this.products[productIndex] = {
-          id,
-          ...updatedProduct
-        };
-        console.log("Producto actualizado:", this.products[productIndex]);
-      } else {
-        console.log("Producto no encontrado.");
-      }
-    }
-  
-    deleteProduct(id) {
-      const productIndex = this.products.findIndex(p => p.id === id);
-      if (productIndex !== -1) {
-        const deletedProduct = this.products.splice(productIndex, 1)[0];
-        console.log("Producto eliminado:", deletedProduct);
-      } else {
-        console.log("Producto no encontrado.");
-      }
+    } catch {
+      console.log(`el archivo ${this.path} no existe, creando...`);
+      await fs.writeFile(this.path, "[]");
+      return [];
     }
   }
-  
-  module.exports = ProductManager;
-  
+
+  getProducts = async () => {
+    const json = await fs.readFile(this.path, "utf-8");
+    this.productos = JSON.parse(json);
+    return this.productos;
+  };
+
+  async addProduct(product) {
+    const json = await fs.readFile(this.path, "utf-8");
+    this.productos = JSON.parse(json);
+    const { title, description, price, thumbnail, code, stock } = product;
+
+    const itsValid = this.productos.some(
+      (productFind) => productFind.code === code
+    );
+    if (itsValid) {
+      console.log(`ERROR: Code in use in ${product.title}`);
+      return;
+    }
+
+    const productn = new Product(product);
+
+    this.productos.push({
+      id: this.id++,
+      ...productn,
+    });
+
+    const newProduct = JSON.stringify(this.productos, null, 2);
+    await fs.writeFile(this.path, newProduct);
+
+    return `producto ${title} ingresado correctamente`;
+  }
+
+  async getProductById(id) {
+    const json = await fs.readFile(this.path, "utf-8");
+    this.productos = JSON.parse(json);
+    const getProduct = this.productos.find((prod) => prod.id === id);
+    if (getProduct) {
+      return getProduct;
+    }
+    return "Producto Not found";
+  }
+  async updateProduct(id, product) {
+    const json = await fs.readFile(this.path, "utf-8");
+    this.productos = JSON.parse(json);
+    const { title, description, price, thumbnail, code, stock } = product;
+
+    const itsValid = this.productos.some((productFind) => productFind.id === id);
+    if (!itsValid) {
+      console.log(`ERROR: ID in use in ${product.title}`);
+      return;
+    }
+
+    let update = this.productos.map((p) => {
+      if (p.id === id) {
+        return { ...p, ...product };
+      }
+      return p;
+    });
+    await fs.writeFile(this.path, JSON.stringify(update, null, 2));
+  }
+
+  async deleteProduct(id) {
+    const json = await fs.readFile(this.path, "utf-8");
+    this.productos = JSON.parse(json);
+    const index = this.productos.findIndex((producto) => producto.id === id);
+    if (index < 0) {
+      return "Producto Not found";
+    }
+
+    this.productos.splice(index, 1);
+    const newProducto = JSON.stringify(this.productos);
+    await fs.writeFile(this.path, newProducto);
+    return "Producto Eliminado Exitosamente";
+  }
+}
+
+class Product {
+  constructor(product) {
+    const { title, description, price, thumbnail, code, stock } = product;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+  }
+}
+const productManager = new ProductManager();
--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 import express from "express";
 import ProductManager from "./ProductManager.js";
-const app = express();
-app.use(express.urlencoded({ extended:true}));
-app.use(express.json());
-const manager = new ProductManager(".products.json");
+const app = express(); 
+const manager = new ProductManager();
 
-app.get("/products", async (rq, res) => {
-try {
-    const limit = req.query.limit;
-    const products = await ProductManager.getAllProducts();
-     res.send(products);
-}
+app.get("/productos", async (req, res) => {
+
+    const {limit} = req.query;
+    const productos = await manager.getProducts();
+    if(limit){
+        res.send(productos.splice(0, +limit));
+    }else {
+       res.send(productos);
+    }
+    
 });
 
-app.get('/products/:pid', async (req, res) => {
-    try {
-      const productId = req.params.pid;
-    }
+app.get("products/:pid", async (req, res) => {
+      const {id:pid} = req.params;
+    const productos = await manager.getProductById(+pid);
+    res.send(productos);
 });
 
 app.listen(8080, () => {
-    console.log("servidor 8080");
+    console.log("conectados");
 });
